3e907688fbf75746b633923f7e3248061347179f
adigitX <adityakammati.workspace@gmail.com>
2026-01-15 11:10:47 +0530

    added notes , with parent child mini shell code

--- lab2.c
+++ lab2.c
@@ -24,20 +24,72 @@
 // }
 
 // pointer - which stores the address of another variable
-// strtok - we have to pass cm, strtok- returns first address of token! (its scans the string , where ever it finds,[space, \n -(enter), tab] it creates the token, and first address of that token is returned!)
+// strtok - we have to pass cmd, strtok- returns first address of token! (its scans the string , where ever it finds,[space, \n -(enter), tab] it creates the token, and first address of that token is returned!)
 // strtok(NULL, " \n\t") - it continues from the last position where it left off!
+
+// #include <stdio.h>
+// int main (){
+//     int x = 15;   // declared an integer variable
+//     int *p;       // declared a pointer variable
+//     p = &x;       // assigned the address of x to pointer p
+//     printf("Value of x = %d\n", x);          // prints the value of x
+//     printf("Address of x = %p\n", &x);       // prints the address of x
+//     printf("Value of p = %p\n", &p);          // prints the address of pointer p
+//     printf("Address stored in p = %p\n", p); // prints the address stored in pointer p 
+//     printf("Value pointed to by p = %d\n", *p); // re-differencing pointer p to get the value of x
+//     *p = 17;
+//     printf("x val = %d\n", x);
+//     printf( "x val via p val = %d\n", *p);
+//     return 0; //signals that program ended successfully
+// }
+
+
 #include <stdio.h>
-int main (){
-    int x = 15;   // declared an integer variable
-    int *p;       // declared a pointer variable
-    p = &x;       // assigned the address of x to pointer p
-    printf("Value of x = %d\n", x);          // prints the value of x
-    printf("Address of x = %p\n", &x);       // prints the address of x
-    printf("Value of p = %p\n", &p);          // prints the address of pointer p
-    printf("Address stored in p = %p\n", p); // prints the address stored in pointer p 
-    printf("Value pointed to by p = %d\n", *p); // prints the value pointed to by pointer p
-    *p = 17;
-    printf("x val = %d\n", x);
-    printf( "x val via p val = %d\n", *p);
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define MAX_LEN 1024
+#define MAX_ARGS 64
+
+void parse(char *cmd, char **args){
+    int i = 0;
+    args[i] = strtok(cmd, " \t\n");
+    while (args[i] && i < MAX_ARGS -1)
+        args[++i] = strtok(NULL, " \t\n");
+    args[i] = NULL;
+}
+
+
+
+// exicute the command using fork + exec
+
+void execute(char **args){
+    pid_t pid = fork();
+
+    if (pid ==0){
+        execvp(args[0], args);
+        perror("command not found");
+        _exit(1);
+    } else{
+        wait(NULL);
+        //parent waits 
+    }
+}
+
+int main() {
+    char cmd[MAX_LEN];
+    char *args[MAX_ARGS];
+    while (1){
+        printf("myshell> ");
+        if (!fgets(cmd, MAX_LEN, stdin))
+            break; // exit on Ctrl+D
+        if (cmd[0]=='\n')
+            continue; // ignore empty input 
+        parse(cmd, args);
+        if (strcmp(args[0], "exit")==0)
+            break; // exit on 'exit' command
+        execute(args);
+        }
     return 0;
 }
